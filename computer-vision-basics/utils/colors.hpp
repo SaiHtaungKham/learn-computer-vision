79062a9f9b23b58f8d8c08155a2a6382f14c9020
Thai Ngoc Tu <thaingoctu@gmail.com>
2018-04-12 13:40:11 +0700

    Add color definitions

---
+++ computer-vision-basics/utils/colors.hpp
@@ -0,0 +1,22 @@
+#ifndef __COLORS_HPP__
+#define __COLORS_HPP__
+
+include "opencv2/opencv.hpp"
+
+using namespace cv;
+
+#define COLOR_BLACK   Scalar(0.0, 0.0, 0.0)
+#define COLOR_WHITE   Scalar(255.0, 255.0, 255.0)
+#define COLOR_BLUE    Scalar(255.0, 0.0, 0.0)
+#define COLOR_GREEN   Scalar(0.0, 255.0, 0.0)
+#define COLOR_RED     Scalar(0.0, 0.0, 255.0)
+#define COLOR_AQUA    Scalar(255.0, 255.0, 0.0)
+#define COLOR_YELLOW  Scalar(0.0, 255.0, 255.0)
+#define COLOR_FUCHSIA Scalar(255.0, 0.0, 255.0)
+#define COLOR_NAVY    Scalar(128.0, 0.0, 0.0)
+#define COLOR_MAROON  Scalar(0.0, 0.0, 128.0)
+#define COLOR_TEAL    Scalar(128.0, 128.0, 0.0)
+#define COLOR_OLIVE   Scalar(0.0, 128.0, 128.0)
+#define COLOR_PURPLE  Scalar(128.0, 0.0, 128.0)
+
+#endif  // __COLORS_HPP__
